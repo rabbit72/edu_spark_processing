aea6f001dcc23c08e90173087c486380dc853d97
Daniil Shadrin <rabbit72rus@gmail.com>
2019-02-13 12:31:18 +0300

    realize functions

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_booked_couples_hotels(data_frame, limit=3):
 
 def get_searched_booked_hotels_from_same_country(data_frame, limit=1):
     """
-   Find the most popular country where hotels are booked and searched from the same country
+   Find N most popular countries where hotels are booked and searched from same country
 
    :param DataFrame data_frame: data with hotel booking and searching
    :param int limit: How many top hotels
@@ -35,7 +35,16 @@ def get_searched_booked_hotels_from_same_country(data_frame, limit=1):
    :rtype: DataFrame
    :raises ValueError: if wrong params # TODO raise exception when wrong data frame
    """
-    raise NotImplementedError
+    cols = ["hotel_country", "user_location_country"]
+    data_frame_result = (
+        data_frame.select(cols)
+        .filter(data_frame.user_location_country == data_frame.hotel_country)
+        .groupBy(cols)
+        .count()
+        .orderBy(col("count").desc())
+    )
+
+    return data_frame_result.limit(limit)
 
 
 def get_searched_hotels_with_children_not_booked(data_frame, limit=3):
@@ -48,7 +57,17 @@ def get_searched_hotels_with_children_not_booked(data_frame, limit=3):
    :rtype: DataFrame
    :raises ValueError: if wrong params # TODO raise exception when wrong data frame
    """
-    raise NotImplementedError
+    cols = ["hotel_continent", "hotel_country", "hotel_market"]
+    data_frame_result = (
+        data_frame.select(cols)
+        .filter(data_frame.srch_children_cnt > 0)
+        .filter(data_frame.is_booking == 0)
+        .groupBy(cols)
+        .count()
+        .orderBy(col("count").desc())
+    )
+
+    return data_frame_result.limit(limit)
 
 
 def main():
